eb65f05e0c39a88f94b60f02c30ecfefd14f9d0a
Marius <52743525+MagicDev-Marius@users.noreply.github.com>
2021-07-14 16:31:16 +0200

    Removed Comments & added task executing

--- src/buildsys.cpp
+++ src/buildsys.cpp
@@ -1,33 +1,29 @@
-﻿#include <fstream>
+#include <fstream>
 #include <string>
 #include <iostream>
 #include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
 
 #include "json.h"
 
 using std::string;
 
-// Little Printer for outputing content to the console
 class Printer {
 public:
-	// write a string to the console
 	void print(string message) {
 		std::cout << message;
 	}
 
-	// write a boolean to the console
-	// (needed for debugging)
 	void print(bool boolean) {
 		std::cout << (boolean ? "true" : "false");
 	}
 
-	// prints a colored message to the console, console is defined by ansii colors
 	void printColored(string message, int color) {
 		print("\x1B[" + std::to_string(color) + "m" + message + "\033[0m\n");
 	}
 };
 
-// read a file by its path and return its content
 string getFileContent(string path) {
 	std::ifstream ifs(path);
 	string content(
@@ -37,7 +33,6 @@ string getFileContent(string path) {
 	return content;
 }
 
-// check if a file with the given path exists
 bool doesFileExists(string path) {
 	FILE* file;
 	if (file = fopen(path.c_str(), "r")) {
@@ -64,12 +59,9 @@ std::tuple<int, JSONCPP_STRING, Json::Value> parseJson(string rawJson) {
 	return std::make_tuple(0, err, root);
 }
 
-// name of the file we want to use as configuration
 const string buildCfgName = ".buildcfg";
-// the "logger" we want to use for outputting data to the console
 const auto printer = new Printer();
 
-// constants for the colors we use in the console
 const auto COLOR_MAGENTA = 35;
 const auto COLOR_RED = 31;
 const auto COLOR_GREEN = 32;
@@ -81,7 +73,13 @@ int main(int argc, char** argv) {
 
 	string dir = ".";
 	if (argc == 1) {
-		auto cwd = argv[0];
+		char buffer[PATH_MAX];
+		if (getcwd(buffer, sizeof(buffer)) == NULL) {
+			printer->printColored("Cannot fetch current working dir", COLOR_RED);
+			return 1;
+		}
+		auto cwd = buffer;
+		
 		if (doesFileExists(cwd)) {
 			dir = cwd;
 		}
@@ -101,18 +99,55 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 
-	// raw JSON
 	auto buildConfigContent = getFileContent(cfgPath);
-	// parsed JSON
 	auto [ status, error, value ] = parseJson(buildConfigContent);	
 	
-	// check if parsing was successfull
 	if (status != 0) {
 		printer->printColored("Error: " + error, COLOR_RED);
 		return 1;
 	}
 
-
+	printer->printColored("> BuildScript " + value["name"].asString() + " v" + value["version"].asString());
+	try {
+		printer->printColored("by " + value["author"].asString(), COLOR_GREEN);
+	} catch (std::exception e) {}
+	try {
+		printer->printColored("=> " + value["description"].asString(), COLOR_GREEN);
+	} catch (std::exception e) {}
+	
+	try {
+		Json::Value jobs = value["jobs"];
+		for (Json::Value::ArrayIndex i = 0; i != jobs.size(); i++) {
+			Json::Value job = jobs[i];
+			try {
+				string tag = std::to_string(i);
+				if (i < 10) {
+					tag = "0" + tag;
+				}
+				printer->printColored("# Running task " + tag + " '" + job["name"].asString() + "'");
+				
+				Json::Value tasks = job["tasks"];
+				for (Json::Value::ArrayIndex j = 0; j != tasks.size(); j++) {
+					Json::Value taskValue = tasks[j];
+					string task = taskValue.asString();
+					
+					printer->printColored("$> " + task, COLOR_WHITE);
+					
+					const char *str = task.c_str();
+					auto result = system(str);
+					
+					if (result != 0) {
+						printer->printColored("!!! Tasks finished with exit code " + std::to_string(result) + ". Exit...", COLOR_WHITE);
+						return 1;
+					}
+				}
+			} catch (std::exception e1) {
+				printer->printColored("!!! Error fetching tasks from job '" + job["name"].asString() + "'. Array might not be valid", COLOR_RED);
+			}	
+		}
+	} catch (std::exception e2) {
+		printer->printColored("!!! Error fetching jobs. Array might not be valid", COLOR_RED);	
+	}
 
 	return 0;
 }
